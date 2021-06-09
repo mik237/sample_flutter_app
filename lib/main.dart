e6597b35f98da8dbdeef7fb48bb2b14edd296be9
muhammad ibrahim <muhammad.ibrahim@innovativesolutions.ae>
2021-06-09 11:48:13 +0400

    Scaffold widget added. AppBar inserted.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() =>  runApp(MyApp());
-
-
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("Sample Flutter App"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("First App"),
+        ),
+      ),
     );
   }
 }
