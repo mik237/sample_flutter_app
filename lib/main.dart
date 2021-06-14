e6f7f74dd156444d300f647ef89ad7da89a364df
muhammad ibrahim <muhammad.ibrahim@innovativesolutions.ae>
2021-06-14 18:36:00 +0400

    debug banner set to false.

--- lib/main.dart
+++ lib/main.dart
@@ -61,6 +61,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.orange),
       home: Scaffold(
         appBar: AppBar(
